f7fec47215b552a970e0c7981a8a44ca478c5f12
ZorAnderius <119595649+ZorAnderius@users.noreply.github.com>
2024-09-29 21:43:00 +0100

    Hw2 mongodb (#2)
    * add mongodb service and search GEt queries
    
    * update readme with render link HW2
    
    * update readme with render link HW2
    
    * update error message
    
    * change readme and delete big alignments

---
+++ src/constants/connection.js
@@ -0,0 +1,9 @@
+export const connect = Object.freeze({
+  PORT: 'PORT',
+  MONGO: {
+    USER: 'MONGODB_USER',
+    PASSWORD: 'MONGODB_PASSWORD',
+    URL: 'MONGODB_URL',
+    DB: 'MONGODB_DB',
+  },
+});
