40cce0d3b0583520bc0f8e37e640a7c5fa52d703
ZorAnderius <gabriel.zorander@gmail.com>
2024-10-08 12:59:10 +0100

    refactoring routes and app

---
+++ src/app.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino';
+import pinoMiddleware from 'pino-http';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import router from './routers/contacts.js';
+
+export const logger = pino({
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+      translateTime: 'SYS:standard',
+      ignore: 'req,res,hostname,pid',
+      messageFormat: '{msg}',
+    },
+  },
+});
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(pinoMiddleware({ logger }));
+
+app.use('/contacts', router);
+
+app.use('*', notFoundHandler);
+app.use(errorHandler);
+
+export default app;
--- src/server.js
+++ src/server.js
@@ -1,25 +1,10 @@
-import express from 'express';
-
 import { env } from './utils/env.js';
 import { connect } from './constants/connection.js';
-import { initExpressSetUp } from './middlewares/initExpressSetUp.js';
-import { errorHandler } from './middlewares/errorHandler.js';
-import { notFoundHandler } from './middlewares/notFoundHandler.js';
-import router from './routers/contacts.js';
+import app, { logger } from './app.js';
 
 const PORT = Number(env(connect.PORT, '3000'));
 
 export const setupServer = () => {
-  const app = express();
-
-  const logger = initExpressSetUp(app);
-
-  app.use(router);
-
-  app.use('*', notFoundHandler);
-
-  app.use(errorHandler);
-
   app.listen(PORT, () => {
     logger.info(`Server is running on the port ${PORT}`);
   });
