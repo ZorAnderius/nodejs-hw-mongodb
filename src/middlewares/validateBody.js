a1d547e83d404378e90f1dfddd8982b8e2ec2bdb
ZorAnderius <gabriel.zorander@gmail.com>
2024-10-18 10:35:15 +0100

    fixe isFavourite validation issues

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -1,12 +1,13 @@
 export const validateBody = (schema) => async (req, res, next) => {
   try {
-    const isFavourite = req.body.isFavourite;
-    req.body.isFavourite =
-      isFavourite === 'true'
-        ? true
-        : isFavourite === 'false'
-        ? false
-        : isFavourite;
+    if (req.body.isFavourite) {
+      req.body.isFavourite =
+        req.body.isFavourite === 'true'
+          ? true
+          : req.body.isFavourite === 'false'
+          ? false
+          : req.body.isFavourite;
+    }
     await schema.validateAsync(req.body, {
       convert: false,
       abortEarly: false,
