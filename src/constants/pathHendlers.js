8dc9f40161728f1daaffa25346b65f2168388893
ZorAnderius <gabriel.zorander@gmail.com>
2024-10-18 10:55:18 +0100

    update temp and upload directories path

--- src/constants/pathHendlers.js
+++ src/constants/pathHendlers.js
@@ -2,6 +2,6 @@ import path from 'node:path';
 
 export const TEMPLATE_DIR = path.join(process.cwd(), 'src', 'templates');
 
-export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
+export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'src', 'temp');
 
-export const UPLOAD_DIR = path.join(process.cwd(), 'upload');
+export const UPLOAD_DIR = path.join(process.cwd(), 'src', 'upload');
